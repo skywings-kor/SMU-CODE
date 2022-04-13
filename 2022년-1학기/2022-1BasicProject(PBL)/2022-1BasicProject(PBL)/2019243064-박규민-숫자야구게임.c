0a26e8dd164655949d683faacfa1b3e015fe5df7
박규민 <pkm229125@naver.com>
2022-04-13 21:03:17 +0900

    추가공부

--- 2022년-1학기/2022-1BasicProject(PBL)/2022-1BasicProject(PBL)/2019243064-박규민-숫자야구게임.c
+++ 2022년-1학기/2022-1BasicProject(PBL)/2022-1BasicProject(PBL)/2019243064-박규민-숫자야구게임.c
@@ -10,7 +10,7 @@
 //	struct node* next;
 //}NODE;
 //
-//void randnumin(NODE* in_list);
+//NODE* randnumin(NODE* in_list);
 //
 //
 //
@@ -21,9 +21,9 @@
 //	int userin;
 //	int res5,res4, res3, res2, res1;
 //
-//	randnumin(list);
+//	list=randnumin(list);
 //
-//	printf("%d %d %d %d %d", list->correct, list->next->correct, list->next->next->correct, list->next->next->next->correct, list->next->next->next->next->correct);
+//	printf("%d %d",list->correct);
 //
 //	scanf("%d", &userin);
 //
@@ -47,25 +47,52 @@
 //
 //}
 //
-//void randnumin(NODE* in_list)
+//NODE* randnumin(NODE* in_list)
 //{
 //	int count=0;
 //	NODE* current = NULL, * follow = NULL;
-//	NODE* newnode= (NODE*)malloc(sizeof(NODE));
+//	NODE* newnode;
+//	current = follow = in_list;
+//	
+//
+//	int temp = 0;
 //
 //	follow = in_list;
 //	current = in_list;
 //
 //	printf("지금부터 랜덤값을 넣습니다.");
-//	srand(time(NULL));
+//
 //	for (int i = 1; ; i++)
 //	{
 //		if (count <= 5)
 //		{
+//			
+//			if (current->next == NULL)
+//			{
+//				if (current == in_list)
+//				{
+//					newnode = (NODE*)malloc(sizeof(NODE));
+//					temp = (rand() % 9) + 1;
+//					newnode->correct = temp;
+//					follow->next = newnode;
+//					newnode->next = current;
+//					count++;
+//				}
+//				newnode = (NODE*)malloc(sizeof(NODE));
+//				temp = (rand() % 9) + 1;
+//				newnode->correct = temp;
+//				follow->next = newnode;
+//				newnode->next = current;
+//				count++;
+//			}
 //
-//			newnode->correct = rand() % 9 + 1;
-//			newnode->next = current;
-//			count++;
+//			else
+//			{
+//				follow = current;
+//				current = current->next;
+//			}
+//
+//
 //		}
 //
 //		else
