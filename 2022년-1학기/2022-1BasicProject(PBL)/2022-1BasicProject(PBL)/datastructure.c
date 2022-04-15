602acbef749bd9004ad2425ba01a5394d28ee8ef
박규민 <pkm229125@naver.com>
2022-04-15 21:19:53 +0900

    4.15study

--- 2022년-1학기/2022-1BasicProject(PBL)/2022-1BasicProject(PBL)/datastructure.c
+++ 2022년-1학기/2022-1BasicProject(PBL)/2022-1BasicProject(PBL)/datastructure.c
@@ -1,26 +1,26 @@
-#include <stdio.h>
-#include<stdlib.h>
-
-//int main(void) {
-//    int a[15][50];
+//#include <stdio.h>
+//#include<stdlib.h>
 //
-//    //a[9][4] = 15;
-//    //*(a[0] + 454) = 15;
-//    //*(&a[9][0] + 4) = 15;
-//    //*(&a[0][0] + 904) = 15;       잘못된 입력!!
-//    
-//    printf("%d", a[9][4]);
-//    
-//    
-//    return 0;
+////int main(void) {
+////    int a[15][50];
+////
+////    //a[9][4] = 15;
+////    //*(a[0] + 454) = 15;
+////    //*(&a[9][0] + 4) = 15;
+////    //*(&a[0][0] + 904) = 15;       잘못된 입력!!
+////    
+////    printf("%d", a[9][4]);
+////    
+////    
+////    return 0;
+////
+////}
 //
+//int main()
+//{
+//	char bbb[10]="abcd";
+//	char* str = (char*)bbb;
+//
+//	printf("%s", bbb);
+//	return 0;
 //}
-
-int main()
-{
-	char bbb[10]="abcd";
-	char* str = (char*)bbb;
-
-	printf("%s", bbb);
-	return 0;
-}
