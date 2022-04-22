44780929b62f9fb83983fec032d16c6eaf22a262
박규민 <pkm229125@naver.com>
2022-04-22 22:08:27 +0900

    숫자야구게임 기본 세팅 완료

--- 2022년-1학기/2022-1BasicProject(PBL)/2022-1BasicProject(PBL)/2019243064-박규민-숫자야구게임.c
+++ 2022년-1학기/2022-1BasicProject(PBL)/2022-1BasicProject(PBL)/2019243064-박규민-숫자야구게임.c
@@ -1,141 +1,245 @@
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <string.h>
-//#include <time.h>
-//#pragma warning (disable:4996)
-//
-//typedef struct node 
-//{
-//	int correct;
-//	struct node* next;
-//}NODE;
-//
-//NODE* randnumin(NODE* in_list);
-//
-//
-//
-//int main(void)
-//{
-//	
-//	NODE* list = NULL;
-//	int userin;
-//	int res5,res4, res3, res2, res1;
-//
-//	list=randnumin(list);
-//
-//	printf("%d %d",list->correct);
-//
-//	scanf("%d", &userin);
-//
-//	res5 = userin / 10000;
-//	userin = userin - res5 *10000;
-//
-//	res4 = userin / 1000;
-//	userin = userin - res4 *1000;
-//
-//	res3 = userin / 100;
-//	userin = userin - res3 *100;
-//
-//	res2 = userin / 10;
-//	userin = userin - res2 *10;
-//	
-//	res1 = userin;
-//
-//	printf("%d %d %d %d %d", res5, res4, res3, res2, res1);
-//
-//
-//
-//}
-//
-//NODE* randnumin(NODE* in_list)
-//{
-//	int count=0;
-//	NODE* current = NULL, * follow = NULL;
-//	NODE* newnode;
-//	current = follow = in_list;
-//	
-//
-//	int temp = 0;
-//
-//	follow = in_list;
-//	current = in_list;
-//
-//	printf("지금부터 랜덤값을 넣습니다.");
-//
-//	for (int i = 1; ; i++)
-//	{
-//		if (count <= 5)
-//		{
-//			
-//			if (current->next == NULL)
-//			{
-//				if (current == in_list)
-//				{
-//					newnode = (NODE*)malloc(sizeof(NODE));
-//					temp = (rand() % 9) + 1;
-//					newnode->correct = temp;
-//					follow->next = newnode;
-//					newnode->next = current;
-//					count++;
-//				}
-//				newnode = (NODE*)malloc(sizeof(NODE));
-//				temp = (rand() % 9) + 1;
-//				newnode->correct = temp;
-//				follow->next = newnode;
-//				newnode->next = current;
-//				count++;
-//			}
-//
-//			else
-//			{
-//				follow = current;
-//				current = current->next;
-//			}
-//
-//
-//		}
-//
-//		else
-//		{
-//			break;
-//		}
-//		
-//
-//	}
-//
-//	return in_list;
-//
-//}
-//
-////#include <time.h>
-////#include<stdio.h>
-////#include<stdlib.h>
-////
-////int main(void)
-////{
-////	/*srand(time(NULL));*/		//난수 초기화
-////	//int num = rand() % 3+1;		// 3을 넣을경우->  0~2 사이에서 뽑게 됨			3+1을 넣을경우-> 1~3 사이에서 뽑게 됨
-////
-////
-////	printf("난수 초기화 이전..\n");
-////	for (int i = 0; i < 10; i++)
-////	{
-////		printf("%d ", rand() % 10);
-////	}
-////
-////	printf("\n\n");
-////
-////
-////	srand(time(NULL));				//중요성!
-////	printf("난수 초기화 이후..\n");
-////	for (int i = 0; i < 10; i++)
-////	{
-////		printf("%d ", rand() % 10);
-////	}
-////
-////	return 0;
-//// 
-//// 
-//// 
-////}
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#pragma warning (disable:4996)
+
+typedef struct node 
+{
+	int correct;
+	struct node* next;
+}NODE;
+
+NODE* randnumin(NODE* in_list);
+NODE* viewnode(NODE* walker);
+NODE* strikecorrectcheck(NODE*list,int f1,int f2,int f3,int f4,int f5,int strikecount);		//노드와 유저가 입력한 값 5개 받아와서 정답 맞는지 확인하는 함수
+NODE* ballcorrectcheck(NODE*list,int f1,int f2,int f3,int f4,int f5,int ballcount);		//노드와 유저가 입력한 값 5개 받아와서 정답 맞는지 확인하는 함수
+
+int main(void)
+{
+	
+	NODE* list;
+	list= NULL;		//노드 리스트 초기화
+	int userin;		//유저가 5자리 입력한 값
+	int res5,res4, res3, res2, res1;		//사용자가 5자리 수 입력한거 나누기 위한 임시 변수
+	int ballcount = 0;		//볼카운트
+	int strikecount = 0;		//스트라이크 카운트
+	list=randnumin(list);
+	
+	viewnode(list);
+
+	for (int i = 0;; i++)
+	{
+		ballcount = 0;
+		strikecount = 0;
+		//유저가 다시 입력했을 때 기존 값이 남아있으면 안되니 초기화 해주기 위한 것
+	
+
+		printf("\n\n 맞추실 숫자5자리를 입력(1~9)->");
+		scanf("%d", &userin);
+		if (99999 < userin)
+		{
+			printf("<error> 숫자가 잘못 입력되었습니다.");
+		}
+
+		else if (userin == 0)
+		{
+			printf("프로그램을 종료합니다");
+			exit(0);
+		}
+		else
+		{
+			res5 = userin / 10000;
+			userin = userin - res5 * 10000;
+
+			res4 = userin / 1000;
+			userin = userin - res4 * 1000;
+
+			res3 = userin / 100;
+			userin = userin - res3 * 100;
+
+			res2 = userin / 10;
+			userin = userin - res2 * 10;
+
+			res1 = userin;
+			printf("%d %d %d %d %d", res5, res4, res3, res2, res1);
+
+			strikecount = strikecorrectcheck(list, res5, res4, res3, res2, res1, ballcount, strikecount);
+			ballcount = ballcorrectcheck(list, res5, res4, res3, res2, res1, ballcount, strikecount);
+			printf("\n\n%d스트라이크 %d볼입니다.", strikecount, ballcount);
+
+			system("pause");
+		}
+		
+
+
+	}
+
+
+}
+
+NODE* randnumin(NODE* in_list)
+{
+	int count=0;
+	NODE* current = NULL, * follow = NULL,*checker=NULL;
+	NODE* newnode;
+	
+
+	int temp = 0;
+
+	srand(time(NULL));
+	printf("지금부터 랜덤값을 넣습니다.");
+
+	for (int i = 1; i<=5; i++)
+	{
+		checker = in_list;		//노드 돌아다니면 값 같을 경우 랜덤함수 다시 하도록 하는거
+
+		follow = in_list;
+		current = in_list;
+		newnode = (NODE*)malloc(sizeof(NODE));
+
+		temp = (rand() % 9) + 1;
+		while (checker != NULL)
+		{
+			if (checker->correct == temp)
+			{
+				temp = (rand() % 9) + 1;
+			}
+
+			checker = checker->next;
+		}
+
+
+		
+		newnode->correct = temp;
+
+		while (current != NULL)
+		{
+			
+			follow = current;
+			current = current->next;
+		}
+		newnode->next = current;
+		if (current == in_list)
+		{
+			in_list = newnode;
+		}
+
+		else
+		{
+			follow->next = newnode;
+		}
+		
+	}
+
+	return in_list;
+
+}
+
+NODE* viewnode(NODE* walker)
+{
+	if (walker != NULL)
+	{
+		printf("%d ", walker->correct);
+		viewnode(walker->next);
+	}
+
+	else
+	{
+		return walker;
+	}
+}
+
+NODE* strikecorrectcheck(NODE* list, int f1, int f2, int f3, int f4, int f5,int strikecount)	//노드와 유저가 입력한 값 5개 받아와서 정답 맞는지 확인하는 함수
+{
+	NODE* walker;
+	walker = list;
+
+	//첫번째자리
+	if (walker->correct == f1)
+	{
+		strikecount = strikecount + 1;
+	}
+
+	walker = walker->next;
+
+	//두번째자리
+	if (walker->correct == f2)
+	{
+		strikecount = strikecount + 1;
+	}
+
+	walker = walker->next;
+
+	//세번째자리
+	if (walker->correct == f3)
+	{
+		strikecount = strikecount + 1;
+	}
+
+	walker = walker->next;
+
+	//네번째자리
+	if (walker->correct == f4)
+	{
+		strikecount = strikecount + 1;
+	}
+
+	walker = walker->next;
+
+	//다섯번째자리
+	if (walker->correct == f5)
+	{
+		strikecount = strikecount + 1;
+	}
+
+
+	return strikecount;
+}
+
+NODE* ballcorrectcheck(NODE* list, int f1, int f2, int f3, int f4, int f5, int ballcount)
+{
+	NODE* walker;
+	walker = list;
+
+	//첫번째자리
+	if ((walker->correct == f2)||(walker->correct==f3) || (walker->correct == f4) || (walker->correct == f5))
+	{
+		ballcount = ballcount + 1;
+	}
+
+	walker = walker->next;
+
+	//두번째자리
+	if ((walker->correct == f1) || (walker->correct == f3) || (walker->correct == f4) || (walker->correct == f5))
+	{
+		ballcount = ballcount + 1;
+	}
+
+	walker = walker->next;
+
+	//세번째자리
+	if ((walker->correct == f2) || (walker->correct == f1) || (walker->correct == f4) || (walker->correct == f5))
+	{
+		ballcount = ballcount + 1;
+	}
+
+	walker = walker->next;
+
+	//네번째자리
+	if ((walker->correct == f2) || (walker->correct == f3) || (walker->correct == f1) || (walker->correct == f5))
+	{
+		ballcount = ballcount + 1;
+	}
+
+	walker = walker->next;
+
+	//다섯번째자리
+	if ((walker->correct == f2) || (walker->correct == f3) || (walker->correct == f4) || (walker->correct == f1))
+	{
+		ballcount = ballcount + 1;
+	}
+
+
+	return ballcount;
+}
--- 2022년-1학기/2022-1BasicProject(PBL)/2022-1BasicProject(PBL)/datastructure.c
+++ 2022년-1학기/2022-1BasicProject(PBL)/2022-1BasicProject(PBL)/datastructure.c
@@ -1,55 +1,55 @@
-//#include <stdio.h>
-//#include<stdlib.h>
-//
-////int main(void) {
-////    int a[15][50];
+////#include <stdio.h>
+////#include<stdlib.h>
 ////
-////    //a[9][4] = 15;
-////    //*(a[0] + 454) = 15;
-////    //*(&a[9][0] + 4) = 15;
-////    //*(&a[0][0] + 904) = 15;       잘못된 입력!!
-////    
-////    printf("%d", a[9][4]);
-////    
-////    
-////    return 0;
+//////int main(void) {
+//////    int a[15][50];
+//////
+//////    //a[9][4] = 15;
+//////    //*(a[0] + 454) = 15;
+//////    //*(&a[9][0] + 4) = 15;
+//////    //*(&a[0][0] + 904) = 15;       잘못된 입력!!
+//////    
+//////    printf("%d", a[9][4]);
+//////    
+//////    
+//////    return 0;
+//////
+//////}
 ////
+////int main()
+////{
+////	char bbb[10]="abcd";
+////	char* str = (char*)bbb;
+////
+////	printf("%s", bbb);
+////	return 0;
 ////}
 //
+//#include <stdio.h>
+//#include <stdlib.h>	
+//#include <string.h>
+//#pragma warning (disable:4996)
+//int ret(int n);
 //int main()
 //{
-//	char bbb[10]="abcd";
-//	char* str = (char*)bbb;
-//
-//	printf("%s", bbb);
-//	return 0;
+//	ret(4);
 //}
-
-#include <stdio.h>
-#include <stdlib.h>	
-#include <string.h>
-#pragma warning (disable:4996)
-int ret(int n);
-int main()
-{
-	ret(4);
-}
-
-int ret(int n)
-{
-	printf("%d \n", n);
-	if (n == 0)
-	{
-		return 0;
-	}
-
-	else if (n == 1)
-	{
-		return 1;
-	}
-	else
-	{
-		return (ret(n - 1) + ret(n - 2));
-	}
-
-}
+//
+//int ret(int n)
+//{
+//	printf("%d \n", n);
+//	if (n == 0)
+//	{
+//		return 0;
+//	}
+//
+//	else if (n == 1)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return (ret(n - 1) + ret(n - 2));
+//	}
+//
+//}
