2d4f5cf7f69373ebcfed1ec34c4d605e02f94cac
박규민 <pkm229125@naver.com>
2022-05-13 01:32:43 +0900

    test

--- 2022년-1학기/2022-1BasicProject(PBL)/2022-1BasicProject(PBL)/2019243064-박규민-RFID데이터분석프로그램.c
+++ 2022년-1학기/2022-1BasicProject(PBL)/2022-1BasicProject(PBL)/2019243064-박규민-RFID데이터분석프로그램.c
@@ -11,27 +11,28 @@ typedef struct taginfo
 	struct taginfo* next;
 } TAGINFO;
 
-TAGINFO* referenceRead(TAGINFO*tagreadlist, char referenceid[27]);
-TAGINFO* referenceInlist(TAGINFO* taglist, char inid[27], double intime, float inrssi);
+TAGINFO* referenceRead(TAGINFO*tagreadlist, char referenceid[27]);		//참조값의 데이터들을 파일에서 가져와 분석하는 함수
+TAGINFO* referenceInlist(TAGINFO* taglist, char inid[27], double intime, float inrssi);		//referenceRead에서 분석을 마친 데이터들이 reference의 연결리스트에 저장되도록 하는 함수
 
-TAGINFO* targetRead(TAGINFO* tagreadlist, char targetid[27]);
-TAGINFO* targetInlist(TAGINFO* taglist, char inid[27], double intime, float inrssi);
+TAGINFO* targetRead(TAGINFO* tagreadlist, char targetid[27]);		//타겟의 데이터를 파일에서 가져와 분석하는 함수
+TAGINFO* targetInlist(TAGINFO* taglist, char inid[27], double intime, float inrssi);		//targetRead에서 분석을 마친 데이터가 target의 연결리스트에 저장되도록 하는 함수
 
-TAGINFO* estimationRead(TAGINFO* estimationlist, TAGINFO* referencelist, float targetrssi);
+TAGINFO* estimationRead(TAGINFO* estimationlist, TAGINFO* referencelist, float targetrssi);		//타겟 ID와 RSSI가 같은 참조데이터 순으로 estimation의 연결리스트에 저장되도록 하는 함수
 
 
-void estimationCal(int inxres,int inyres, char finalid[27],int rescount);
+void estimationCal(int inxres,int inyres, char finalid[27],int rescount);		//타겟의 최종 예측 좌표를 계산하고 출력하는 함수
 
-TAGINFO* estimationview(TAGINFO* estimationlist);
-TAGINFO* listInview(TAGINFO* walker);
+TAGINFO* estimationview(TAGINFO* estimationlist);		//예측하기 위해 해당하는 데이터들을 저장한 연결리스트에 저장한 값들을 한 번 출력해주는 함수
+TAGINFO* listInview(TAGINFO* walker);		//1번, 2번 기능에서 진행한 것을 출력하기 위한 연결리스트 출력 함수
 
 int main()
 {
 	TAGINFO* referencelist = NULL,*targetlist=NULL,*estimationlist=NULL,*mainwalker=NULL;
-	int interchoice;
+	int interchoice;		//사용자가 사용자 메뉴 선택할 때 들어갈 변수
 	
-	char targetID[27] = "0x35E0170044CF0D590000F5A5";
+	char targetID[27] = "0x35E0170044CF0D590000F5A5";		//타겟 ID
 
+	//참조 ID
 	char referenceIDs[60][27] =
 	{
 		"0x35E0170044CF0D590000F544","0x35E0170044CF0D590000F543","0x35E0170044CF0D590000F542","0x35E0170044CF0D590000F551","0x35E0170044CF0D590000F550","0x35E0170044CF0D590000F54F",
@@ -45,7 +46,8 @@ int main()
 		"0x35E0170044CF0D590000F58C","0x35E0170044CF0D590000F58B","0x35E0170044CF0D590000F58A","0x35E0170044CF0D590000F599","0x35E0170044CF0D590000F598","0x35E0170044CF0D590000F597",
 		"0x35E0170044CF0D590000F603","0x35E0170044CF0D590000F5F8","0x35E0170044CF0D590000F5F7","0x35E0170044CF0D590000F5F6","0x35E0170044CF0D590000F605","0x35E0170044CF0D590000F604"
 	};
-
+	
+	//참조 좌표값
 	int referecePoints[60][2] =
 	{
 		{ 7, 1 },{ 22, 1 },{ 37, 1 },{ 52, 1 },{ 67, 1 },{ 81, 1 },
@@ -60,14 +62,18 @@ int main()
 		{ 101, 144 },{ 116, 144 },{ 131, 144 },{ 146, 144 },{ 161, 144 },{ 176, 144 }
 	};
 
+
+	//메인 인터페이스 시작
 	for (int i = 0;; i++)
 	{
-		system("cls");
+		system("cls");		//화면 싹 깨끗해지게 해주는 기능
+
 		printf("					Made by-2019243064 박규민\n\n\n\n");
 		printf("				<실행하고 싶은 시스템 번호를 입력해주시면 됩니다>\n\n");
 		printf("					1. Reference Tag Analysis\n\n");
 		printf("					2. Target Tag Analysis\n\n");
 		printf("					3. Estimation Tag Analysis\n\n");
+		printf("					4. Target ID 변경하기\n\n");
 		printf("					0. Quit\n\n");
 
 		scanf("%d", &interchoice);
@@ -77,108 +83,128 @@ int main()
 		if (interchoice == 1)		//참조 태그 분석 함수실행
 		{
 			printf("					<참조 태그 데이터가 존재하지 않는 ID목록>\n");
-			referencelist = NULL, targetlist = NULL, estimationlist = NULL;
-			if (referencelist == NULL)
-			{
-				for (int j = 0; j < 60; j++)
-				{
-					referencelist = referenceRead(referencelist, referenceIDs[j]);
+			referencelist = NULL, targetlist = NULL, estimationlist = NULL;		//연결리스트가 기존에 하던것과 같이 계속 쌓이면 안되니까 한 번 싹 비워줍니다.
 
-				}
+			for (int j = 0; j < 60; j++)		//참조 데이터들이 60칸으로 정해져있으니 배열0번째부터 59번째까지 확인하도록 합니다.
+			{
+				referencelist = referenceRead(referencelist, referenceIDs[j]);		//j가 0일경우 참조 ID 들어있는 0번째 배열을 가져와 referenceRead 함수에 보냅니다.(연결리스트 또한 저장을 위해 보내줍니다)
+
 			}
+
 			printf("\n");
 			printf("					<참조 태그 데이터 분석 목록>\n");
-			listInview(referencelist);		//연결리스트에 넣은 참조 데이터 보기 위한 함수
-			
+			listInview(referencelist);		//연결리스트에 넣은 참조 데이터들을 보기 위한 함수
+
 		}
 
 		else if (interchoice == 2)		//타겟 태그 분석 함수실행
 		{
 			printf("					<타겟 태그 데이터가 존재하지 않는 ID목록>\n");
-			referencelist = NULL, targetlist = NULL, estimationlist = NULL;
-			if (targetlist == NULL)
-			{
-				targetlist = targetRead(targetlist, targetID);
-			}
+			referencelist = NULL, targetlist = NULL, estimationlist = NULL;		//연결리스트가 기존에 하던것과 같이 계속 쌓이면 안되니까 한 번 싹 비워줍니다.
+
+			targetlist = targetRead(targetlist, targetID);		//타겟 ID는 1개로 정해져있으므로 1번만 실행시켜줍니다.
+
 			printf("\n");
 			printf("					<타겟 태그 데이터 분석 목록>\n");
-			listInview(targetlist);
+			listInview(targetlist);		//연결리스트에 넣은 타겟 데이터를 보기 위한 함수
 
 
 		}
-		
 
-		else if (interchoice == 3)
+
+		else if (interchoice == 3)		//타겟 태그의 예측 좌표
 		{
-			
-			int xres = 0;
-			int yres = 0;
+			int xres = 0;		//x좌표 총합 변수
+			int yres = 0;		//y좌표 총합 변수
 
-			int esticount = 0;
-			int knnuser = 0;
-			referencelist = NULL, targetlist = NULL, estimationlist = NULL;
+			int esticount = 0;		//참조 ID 배열의 몇번째인지 저장하고 이 횟수를 바탕으로 참조 좌표값을 가져오기 위한 변수
+			int knnuser = 0;		//knn알고리즘을 하기 위해 사용자가 참조 범위를 지정하도록 하기 위한 변수
+
+			referencelist = NULL, targetlist = NULL, estimationlist = NULL;		//기존 데이터에 쌓이면 안되니 한 번 싹 비워줍니다.
+
 			printf("					<참조 태그 데이터가 존재하지 않는 ID목록>\n");
-			if (referencelist == NULL)		//참조 태그 분석 함수실행
-			{
 
-				for (int j = 0; j < 60; j++)
-				{
-					referencelist = referenceRead(referencelist, referenceIDs[j]);
-				}
-			}
-			printf("\n\n");
-			if (targetlist == NULL)		//타겟 태그 분석 함수 실행
+			//참조 태그 분석 함수 실행
+			for (int j = 0; j < 60; j++)
 			{
-				targetlist = targetRead(targetlist, targetID);
+				referencelist = referenceRead(referencelist, referenceIDs[j]);		//1번 기능과 동일
 			}
+			
+			printf("\n\n");
+
+			//타겟 태그 분석 함수 실행
+			targetlist = targetRead(targetlist, targetID);		//2번 기능과 동일
 
 			printf("					<RSSI근접도 분석 목록>\n");
 
-			estimationlist = estimationRead(estimationlist,referencelist,targetlist->rssi);
+			estimationlist = estimationRead(estimationlist, referencelist, targetlist->rssi);		//
 			estimationview(estimationlist);
-			
+
 			printf("참조할 데이터 개수를 입력해주세요: ");
 			scanf("%d", &knnuser);
 
 			if ((knnuser < 1) || (60 < knnuser))
 			{
-				printf("입력하신 개수가 참조 범위를 벗어납니다");
+				printf("error..입력하신 개수가 참조 범위를 벗어납니다");
+				printf("처음으로 돌아갑니다.");
 			}
 
-			mainwalker = estimationlist;
-			int knncount=0;
-
-			for (int f = 0; f <= knnuser - 1; f++)
+			else
 			{
-				esticount = 0;
-				knncount = knncount + 1;
-				for (int e = 0;e<60; e++)
+				mainwalker = estimationlist;
+				int knncount = 0;
+
+				for (int f = 0; f <= knnuser - 1; f++)
 				{
-					if (strcmp(mainwalker->id, referenceIDs[e]) == 0)
+					esticount = 0;
+					knncount = knncount + 1;
+					for (int e = 0; e < 60; e++)
 					{
-						break;
+						if (strcmp(mainwalker->id, referenceIDs[e]) == 0)
+						{
+							break;
+						}
+						else
+						{
+							esticount = esticount + 1;
+						}
+
 					}
-					else
-					{
-						esticount = esticount + 1;
-					}
-					
+
+					xres = referecePoints[esticount][0] + xres;
+					yres = referecePoints[esticount][1] + yres;
+
+
+					mainwalker = mainwalker->next;
+
 				}
 
-				xres = referecePoints[esticount][0] + xres;
-				yres = referecePoints[esticount][1] + yres;
-				
-				printf("%d ", xres);
-				printf("%d\n", yres);
-				mainwalker = mainwalker->next;
-				
+				estimationCal(xres, yres, mainwalker->id, knncount);
+
 			}
-			
-			estimationCal(xres, yres, mainwalker->id, knncount);
+
 
 		}
 
+		else if (interchoice == 4)
+		{
+			char temp[27] = "";
+			printf("변경하실 Target의 ID를 입력(26개 정확히): ");
+			scanf("%s", temp);
 
+			if (strlen(temp) == 26)
+			{
+				strcpy(targetID, temp);
+				printf("타겟 ID의 변경이 완료되었습니다\n\n");
+			}
+		
+			else
+			{
+				printf("Error...잘못입력하셨습니다\n\n");
+			}
+		
+			
+		}
 
 		else if (interchoice == 0)
 		{
