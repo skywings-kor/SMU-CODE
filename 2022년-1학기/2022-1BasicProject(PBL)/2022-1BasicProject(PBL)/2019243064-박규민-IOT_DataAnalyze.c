1d9f2e18199510b6a10128f469c7f067a83baad3
박규민 <pkm229125@naver.com>
2022-05-30 15:17:53 +0900

    plus

--- 2022년-1학기/2022-1BasicProject(PBL)/2022-1BasicProject(PBL)/2019243064-박규민-IOT_DataAnalyze.c
+++ 2022년-1학기/2022-1BasicProject(PBL)/2022-1BasicProject(PBL)/2019243064-박규민-IOT_DataAnalyze.c
@@ -4,7 +4,10 @@
 #include <time.h>
 #pragma warning (disable:4996)
 
-int loginsys(char userid[20], char userpw[30]);
+int loginsys(char userid[20], char userpw[30]);		//로그인함수
+
+int signup(int room, int households, char createid[20], char createpw[30]);		//회원가입함수
+
 
 int main()
 {
@@ -13,6 +16,12 @@ int main()
 	int logincheck;		//로그인 함수에서 반환값 받는 변수
 	char userid[20], userpw[30];
 
+	//회원가입 변수
+	int room;	//호실
+	int households;		//세대원수
+	char createid[20];		//아이디
+	char createpw[30];		//비번
+	int createcheck;		//회원가입이 정상적으로 되었는지 확인
 
 	for (int i = 0; ; i++)
 	{
@@ -39,6 +48,7 @@ int main()
 			if (logincheck == 1)
 			{
 				printf("로그인성공\n\n");
+				system("pause");
 				break;
 
 			}
@@ -52,7 +62,20 @@ int main()
 
 		else if (userchoice1 == 2)
 		{
+			printf("호수를 입력해주세요(ex_1203): ");
+			scanf("%d", &room);
 
+			printf("\n세대원이 몇명인지 입력해주세요(ex_4): ");
+			scanf("%d", &households);
+
+			printf("\n만드실 아이디를 입력해주세요(20자 이내): ");
+			scanf("%s", createid);
+
+			printf("\n만드실 비밀번호를 입력해주세요(20자 이내): ");
+			scanf("%s", createpw);
+
+			createcheck=signup(room, households, createid, createpw);
+			
 		}
 
 		else if (userchoice1 == 3)
@@ -68,7 +91,7 @@ int main()
 
 	}
 
-
+	system("cls");
 
 	printf("로그인에 성공하여 메인페이지로 왔습니다");
 	return 0;
@@ -105,4 +128,38 @@ int loginsys(char userid[20], char userpw[30])
 	return 0;		//로그인 실패
 
 
+}
+
+
+int signup(int room, int households, char createid[20], char createpw[30])
+{
+	
+	FILE* fp;
+	fp = fopen("IOTuser.txt", "a");
+
+	char str[60];
+	char* alid;
+	char* alpw;
+	char* alroom;
+
+	while (!feof(fp))
+	{
+		fgets(str, sizeof(str), fp);
+
+	}
+
+	while (!feof(fp))		//열은 파일에서 마지막 부분까지 나올때까지 반복
+	{
+		fgets(str, sizeof(str), fp);	
+		id = strtok(str, " ");		
+		pw = strtok(NULL, " ");		//위 로그인 코드와 동일한 역할
+
+		if (strcmp(createid, id) == 0)		//같은 아이디가 이미 존재시
+		{
+			return 1;		
+			break;
+		}
+
+
+	}
 }
