ec4f529f8c3f3aa0b2855edcb86e42cb435a09a2
박규민 <pkm229125@naver.com>
2022-06-10 01:15:26 +0900

    기말 공부

--- 2022년-1학기/2022-1BasicProject(PBL)/2022-1BasicProject(PBL)/datastructure.c
+++ 2022년-1학기/2022-1BasicProject(PBL)/2022-1BasicProject(PBL)/datastructure.c
@@ -1,55 +1,126 @@
-////#include <stdio.h>
-////#include<stdlib.h>
-////
-//////int main(void) {
-//////    int a[15][50];
+//////#include <stdio.h>
+//////#include<stdlib.h>
 //////
-//////    //a[9][4] = 15;
-//////    //*(a[0] + 454) = 15;
-//////    //*(&a[9][0] + 4) = 15;
-//////    //*(&a[0][0] + 904) = 15;       잘못된 입력!!
-//////    
-//////    printf("%d", a[9][4]);
-//////    
-//////    
-//////    return 0;
+////////int main(void) {
+////////    int a[15][50];
+////////
+////////    //a[9][4] = 15;
+////////    //*(a[0] + 454) = 15;
+////////    //*(&a[9][0] + 4) = 15;
+////////    //*(&a[0][0] + 904) = 15;       잘못된 입력!!
+////////    
+////////    printf("%d", a[9][4]);
+////////    
+////////    
+////////    return 0;
+////////
+////////}
 //////
+//////int main()
+//////{
+//////	char bbb[10]="abcd";
+//////	char* str = (char*)bbb;
+//////
+//////	printf("%s", bbb);
+//////	return 0;
 //////}
 ////
+////#include <stdio.h>
+////#include <stdlib.h>	
+////#include <string.h>
+////#pragma warning (disable:4996)
+////int ret(int n);
 ////int main()
 ////{
-////	char bbb[10]="abcd";
-////	char* str = (char*)bbb;
-////
-////	printf("%s", bbb);
-////	return 0;
+////	ret(4);
 ////}
+////
+////int ret(int n)
+////{
+////	printf("%d \n", n);
+////	if (n == 0)
+////	{
+////		return 0;
+////	}
+////
+////	else if (n == 1)
+////	{
+////		return 1;
+////	}
+////	else
+////	{
+////		return (ret(n - 1) + ret(n - 2));
+////	}
+////
+////}\
+//
+//
+////기말고사 연습
 //
 //#include <stdio.h>
-//#include <stdlib.h>	
-//#include <string.h>
+//#include <stdlib.h>
 //#pragma warning (disable:4996)
-//int ret(int n);
+//
+//typedef struct Treenode
+//{
+//	int data;
+//	struct Treenode* left, * right;
+//}Treenode;
+//
+//
+//
 //int main()
 //{
-//	ret(4);
+//
+//
+//
+//
+//
+//	return 0;
 //}
 //
-//int ret(int n)
+//
+//void preorder(struct Node* root)
 //{
-//	printf("%d \n", n);
-//	if (n == 0)
+//	struct Node* p;
+//	p = root;
+//	while (p != NULL)
 //	{
-//		return 0;
+//		printf("%d", p->data);
+//		p = p->left;
+//	}
+//}
+//
+//Treenode* search(Treenode* node, int key)
+//{
+//	if (node == NULL)
+//	{
+//		return NULL;
 //	}
 //
-//	else if (n == 1)
-//	{
-//		return 1;
-//	}
+//	if (key == node->key)		//넣으려는 값과 노드가 가리키고 있는 값이 같을 경우엔 끝내고
+//		return node;
+//
+//	else if (key < node->key)	//값이 현재 노드가 가리키고 있는 값보다 작을 경우
+//		return search(node->left, key);		//왼쪽으로 이동
+//
 //	else
+//		return search(node->right, key);		//클경우에는 오른쪽!
+//}
+//
+//Treenode* searchtry(Treenode* node, int key)
+//{
+//	while (node != NULL)		//노드의 마지막 부분까지 반복하고
 //	{
-//		return (ret(n - 1) + ret(n - 2));
+//		if (key == node->key)		//값에서 현재 노드가 가리키고 있는 값 
+//			return node;
+//
+//		else if (key < node->key)
+//			node = node->left;
+//
+//		else
+//			node = node->right;		
 //	}
 //
+//	return NULL;		//탐색 실패면 NULL
 //}
